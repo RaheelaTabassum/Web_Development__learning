3f88d6f3cf37c449cad330463edc736db5d4204a
Raheela <itsraheela@gmail.com>
2023-09-12 20:33:15 +0530

    3rd commit

--- weatherApp.js
+++ weatherApp.js
@@ -1,5 +1,5 @@
 const userTab = document.querySelector("[data-userWeather]");
-const searchTab = document.querySelector("[ata-searchWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
 const userContainer = document.querySelector(".weather-container");
 const grantAccessContainer = document.querySelector(".grant-location-container");
 const SearchForm = document.querySelector("[data-searchForm]");
@@ -16,7 +16,7 @@ currentTab.classList.add("current-tab");
 
 
 function switchTab(clickedTab){ 
-    if(clickedTab != currentTab){
+    if (clickedTab !== currentTab) {
         currentTab.classList.remove("current-tab");
         currentTab = clickedTab;
         currentTab.classList.add("current-tab");
@@ -29,7 +29,7 @@ function switchTab(clickedTab){
         }
         else {
             SearchForm.classList.remove("active");
-            userInfoContainer.remove("active");
+            userInfoContainer.classList.remove("active");
             getfromSessionStorage();
         }
    }
@@ -44,3 +44,31 @@ searchTab.addEventListener("click", () => {
     // pass clicked tab as input parameter
     switchTab(searchTab);
 });
+
+function getfromSessionStorage() {
+    const localCoordinates = sessionStorage.getItem("user-cordinates");
+    if (!localCoordinates) {
+        
+        grantAccessContainer.classList.add("active");
+    }
+    else {
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates)
+    }
+}
+
+async function fetchUserWeatherInfo(coordinates) {
+    const { lat, lon } = coordinates;
+    grantAccessContainer.classList.remove("active");
+    loadingScreen.classList.add("active");
+ 
+    // API CALL
+    try {
+        const response = await fetch(
+           ` https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_Key}`
+        );
+        const 
+           
+    
+    
+}
